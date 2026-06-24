155084b7ec02ac200ed70aebf8e49daf49ed93e5
tgoky <tgokyicloud6@gmail.com>
2026-06-24 11:20:44 +0000

    update stripe version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -15,7 +15,7 @@ function getStripe(): Stripe {
   }
   if (!_stripe) {
     _stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: "2024-11-20.acacia",
+      apiVersion: "2026-03-25.dahlia",
       typescript: true,
     });
   }
